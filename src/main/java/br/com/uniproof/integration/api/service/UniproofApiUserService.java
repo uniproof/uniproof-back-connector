52640261ba3b1a46f2bd2e3078c899e326a409e9
Edgard Leite <napalm.sp@gmail.com>
2022-04-11 16:34:30 -0300

    Complement User Client Services

--- src/main/java/br/com/uniproof/integration/api/service/UniproofApiUserService.java
+++ src/main/java/br/com/uniproof/integration/api/service/UniproofApiUserService.java
@@ -18,76 +18,297 @@ import java.util.List;
 @Slf4j
 public class UniproofApiUserService {
 
-    @Autowired
-    UniproofUserClient uniproofApiClient;
+	@Autowired
+	UniproofUserClient uniproofApiClient;
 
-    public List<LotItem> getLotDocuments(String lotId, String companyToken) {
-        return uniproofApiClient.listLotInfo(lotId, companyToken);
-    }
+	public List<LotItem> getLotDocuments(
+			String lotId,
+			String companyToken) {
+		return uniproofApiClient.listLotInfo(lotId, companyToken);
+	}
 
-    public MultipartFile[] getDocument(String documentId, String token) {
-        return uniproofApiClient.getDocument(documentId, token);
-    }
+	public MultipartFile[] getDocument(
+			String documentId,
+			String token) {
+		return uniproofApiClient.getDocument(documentId, token);
+	}
 
-    public List<br.com.uniproof.integration.api.beans.Service> getServices(String companyToken) {
-        return uniproofApiClient.getServices(companyToken);
-    }
+	public List<br.com.uniproof.integration.api.beans.Service> getServices(
+			String companyToken) {
+		return uniproofApiClient.getServices(companyToken);
+	}
 
-    public List<Company> getCompany() {
-        return uniproofApiClient.getCompanyResult();
-    }
+	public List<Company> getCompany() {
+		return uniproofApiClient.getCompanyResult();
+	}
+
+	public LotItem uploadFileToLot(
+			String lotId,
+			String name,
+			Path file,
+			String companyToken) {
+		LotItem result = null;
+		try {
+			MultipartFile mpfile = new MockMultipartFile(
+					"file",
+					name,
+					"application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+					Files.newInputStream(file));
+
+			result = uniproofApiClient.uploadFileToLot(lotId, mpfile, companyToken).getBody();
+		} catch (FeignException f) {
+			throw f;
+		} catch (Exception ex) {
+			log.error("Falha ao criar arquivo na plataforma: ", ex);
+			throw new RuntimeException(ex);
+		}
+		return result;
+	}
+
+	public void confirmLotSend(
+			List<String> lotIds,
+			String companyToken) {
+		ResponseEntity<List<CartItem>> retorno =
+				uniproofApiClient.confirmSend(
+						CartConfirmRequest.builder().lotIds(lotIds).build(),
+						companyToken);
+	}
+
+	public List<Company> getCompanyResult() {
+		return uniproofApiClient.getCompanyResult();
+	}
+
+	public List<City> listCities(
+			String name,
+			Integer offset,
+			Integer limit,
+			String companyToken) {
+		return uniproofApiClient.listCities(name, offset, limit, companyToken);
+	}
+
+	public List<LotItem> listLotInfo(
+			String lotId,
+			String companyToken) {
+		return uniproofApiClient.listLotInfo(lotId, companyToken);
+	}
+
+	public ResponseEntity<LotItem> uploadFileToLot(
+			String lotId,
+			MultipartFile file,
+			String companyToken) {
+		return uniproofApiClient.uploadFileToLot(
+				lotId,
+				file,
+				companyToken);
+	}
+
+	public LotItem getLotItemInfo(
+			String lotId,
+			String lotItemId,
+			String companyToken) {
+		return uniproofApiClient.getLotItemInfo(
+				lotId,
+				lotItemId,
+				companyToken);
+	}
+
+	public LotItem removeLotItemFromLot(
+			String lotId,
+			String lotItemId,
+			String companyToken) {
+		return uniproofApiClient.removeLotItemFromLot(
+				lotId,
+				lotItemId,
+				companyToken);
+	}
+
+	public String resolveRequirement(
+			String lotId,
+			String lotItemId,
+			String companyToken) {
+		return uniproofApiClient.resolveRequirement(
+				lotId,
+				lotItemId,
+				companyToken);
+	}
+
+	public Lot createLot(
+			Lot lot,
+			String companyToken) {
+		return uniproofApiClient.createLot(
+				lot,
+				companyToken
+		);
+	}
+
+	public Lot updateLot(
+			String lotId,
+			Lot lot,
+			String companyToken) {
+		return uniproofApiClient.updateLot(
+				lotId,
+				lot,
+				companyToken
+		);
+	}
+
+	public FormResponse fillFormJson(
+			String lotId,
+			String jsonForm,
+			String companyToken) {
+		return uniproofApiClient.fillFormJson(
+				lotId,
+				jsonForm,
+				companyToken
+		);
+	}
+
+	public FormResponse fillForm(
+			String lotId,
+			Object form,
+			String companyToken) {
+		return uniproofApiClient.fillForm(
+				lotId,
+				form,
+				companyToken
+		);
+	}
 
 
-    public Lot createLot(Lot lot, String companyToken) {
-        return uniproofApiClient.createLot(lot, companyToken);
-    }
+	public List<Folder> listFolders(
+			String containerId,
+			String companyToken) {
+		return uniproofApiClient.listFolders(
+				containerId,
+				companyToken);
+	}
 
-    public LotItem uploadFileToLot(
-            String lotId,
-            String name,
-            Path file,
-            String companyToken) {
-        LotItem result = null;
-        try {
-            MultipartFile mpfile = new MockMultipartFile(
-                    "file",
-                    name,
-                    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-                    Files.newInputStream(file));
+	public List<Folder> listFolders(
+			String containerId,
+			Integer limit,
+			Integer offset,
+			String companyToken) {
+		return uniproofApiClient.listFolders(
+				containerId,
+				limit,
+				offset,
+				companyToken);
+	}
 
-            result = uniproofApiClient.uploadFileToLot(lotId, mpfile, companyToken).getBody();
-        } catch (FeignException f) {
-            throw f;
-        } catch (Exception ex) {
-            log.error("Falha ao criar arquivo na plataforma: ", ex);
-            throw new RuntimeException(ex);
-        }
-        return result;
-    }
+	public Folder createFolder(
+			Folder folder,
+			String companyToken) {
+		return uniproofApiClient.createFolder(
+				folder,
+				companyToken);
+	}
 
-    public void confirmLotSend(
-            List<String> lotIds,
-            String companyToken) {
+	public List<Lot> listLotsFromFolder(
+			String containerId,
+			String companyToken) {
+		return uniproofApiClient.listLotsFromFolder(
+				containerId,
+				companyToken);
+	}
 
-        ResponseEntity<List<CartItem>> retorno =
-                uniproofApiClient.confirmSend(
-                        CartConfirmRequest.builder().lotIds(lotIds).build(),
-                        companyToken);
+	public Folder getFolder(
+			String folderId,
+			String companyToken) {
+		return uniproofApiClient.getFolder(
+				folderId,
+				companyToken);
+	}
 
-        /*
-        StringBuffer jsonlotids = new StringBuffer();
-        jsonlotids.append("{ \"lotIds\": [ ");
-        lotIds.forEach(id -> {
-            jsonlotids.append("\"")
-                    .append(id)
-                    .append("\",");
-        });
-        jsonlotids.deleteCharAt(jsonlotids.length() - 1);
-        jsonlotids.append("]}");
-        ResponseEntity<String> retorno = uniproofApiClient.confirmLotSend(jsonlotids.toString(), companyToken);
-        return;
 
-         */
-    }
+	public Folder updateFolder(
+			String folderId,
+			Folder folder,
+			String companyToken) {
+		return uniproofApiClient.updateFolder(
+				folderId,
+				folder,
+				companyToken);
+	}
 
+	public Folder removeFolder(
+			String folderId,
+			String companyToken) {
+		return uniproofApiClient.removeFolder(
+				folderId,
+				companyToken);
+	}
+
+	public MultipartFile[] getDocumentVersion(
+			String documentId,
+			String documentVersion,
+			String token) {
+		return uniproofApiClient.getDocumentVersion(
+				documentId,
+				documentVersion,
+				token
+		);
+	}
+
+	public ResponseEntity<LotItem> uploadAttachmentToLotItem(
+			String lotId,
+			MultipartFile file,
+			Integer attachmentTypeId,
+			String companyToken) {
+		return uniproofApiClient.uploadAttachmentToLotItem(
+				lotId,
+				file,
+				attachmentTypeId,
+				companyToken
+		);
+	}
+
+	public List<AttachmentType> listAttachmentType(
+			String companyToken) {
+		return uniproofApiClient.listAttachmentType(
+				companyToken);
+	}
+
+	public List<EventType> listEventType(
+			String companyToken) {
+		return uniproofApiClient.listEventType(
+				companyToken);
+	}
+
+	public ResponseEntity<Wallet> getWalletBalance(
+			String containerId,
+			String companyToken) {
+		return uniproofApiClient.getWalletBalance(
+				containerId,
+				companyToken);
+	}
+
+	public ResponseEntity<List<CartItem>> getCart(
+			String companyToken) {
+		return uniproofApiClient.getCart(
+				companyToken);
+	}
+
+	public ResponseEntity<List<CartItem>> getCartFromFolder(
+			String containerId,
+			String companyToken) {
+		return uniproofApiClient.getCartFromFolder(
+				containerId,
+				companyToken);
+	}
+
+	public ResponseEntity<String> confirmLotSend(
+			String lotIds,
+			String companyToken) {
+		return uniproofApiClient.confirmLotSend(
+				lotIds,
+				companyToken);
+	}
+
+	public ResponseEntity<List<CartItem>> confirmSend(
+			CartConfirmRequest lotItemIds,
+			String companyToken) {
+		return uniproofApiClient.confirmSend(
+				lotItemIds,
+				companyToken);
+	}
 }
