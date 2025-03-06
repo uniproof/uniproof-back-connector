5c09a797590ed387533ee79001eca134b54556cf
Edgard Leite <napalm.sp@gmail.com>
2025-03-06 10:53:45 -0300

    Remoção de prefixo em nome de pasta

--- src/main/java/br/com/uniproof/integration/api/utils/StringUtils.java
+++ src/main/java/br/com/uniproof/integration/api/utils/StringUtils.java
@@ -17,126 +17,291 @@ public class StringUtils {
     public static void main(String[] args) throws JsonProcessingException, InvocationTargetException, IllegalAccessException, NoSuchMethodException {
         String teste = "rTeste";
         Map map = convertObjToMap(teste);
+        String json = """
+                {
+                	"id": "7738edfa-7862-4058-b0f6-7c9aabbd2a78",
+                	"lotId": "e9cf4553-2a9e-4a41-a1eb-0ba546efcb1e",
+                	"creatorId": 2,
+                	"couponId": null,
+                	"currentStatus": "pendent",
+                	"notaryId": 2,
+                	"hasNotification": false,
+                	"notificationCount": 0,
+                	"parentId": null,
+                	"protocol": null,
+                	"startedAt": "2025-02-13T19:17:30.133Z",
+                	"sellingPrice": 0,
+                	"notaryPrice": 0,
+                	"currentVersion": 0,
+                	"registeredVersion": null,
+                	"archivedAt": null,
+                	"currentStatusAt": "2025-02-20T21:16:32.780Z",
+                	"createdAt": "2025-02-13T19:14:53.238Z",
+                	"updatedAt": "2025-02-27T18:45:59.065Z",
+                	"event": {
+                		"id": 8029421,
+                		"userId": 2,
+                		"notaryId": null,
+                		"ownerType": "LotItem",
+                		"ownerId": "7738edfa-7862-4058-b0f6-7c9aabbd2a78",
+                		"status": "pendent",
+                		"description": "",
+                		"archivedAt": null,
+                		"archiverId": null,
+                		"createdAt": "2025-02-20T21:16:32.780Z",
+                		"updatedAt": "2025-02-20T21:16:32.780Z"
+                	},
+                	"owner": {
+                		"id": 4,
+                		"token": "217a43e6-50d6-4121-a8ae-394e9fa54a25",
+                		"userId": null,
+                		"kind": "company",
+                		"standard": false,
+                		"cityId": 3550308,
+                		"name": "Uniproof",
+                		"fantasy": null,
+                		"phone": null,
+                		"shortName": "Uniproof",
+                		"cnpj": "29.383.051/0001-21",
+                		"email": null,
+                		"onlyCommercialAddress": false,
+                		"endContractTerm": null,
+                		"startContractTerm": null,
+                		"comments": null,
+                		"companyId": null,
+                		"superCompany": false,
+                		"notaryIds": [],
+                		"companyIds": [],
+                		"createdAt": "2018-05-24T18:10:40.176Z",
+                		"updatedAt": "2018-05-24T18:10:40.176Z",
+                		"entity": "Company"
+                	},
+                	"service": {
+                		"id": 3,
+                		"name": "REGISTRO DE DOCUMENTOS NATO DIGITAIS JÁ ASSINADOS - RTD",
+                		"identifier": "external",
+                		"alias": "Referente ao Registro do Contrato",
+                		"description": "Registro de documentos nato digitais já contém assinaturas eletrônicas e ou digitais (ICP) das partes.",
+                		"fullDescription": "Registro de documentos nato digitais já contém assinaturas eletrônicas e ou digitais (ICP) das partes.",
+                		"editUrl": "https://df-app-stage.uniproof.com.br/dynamic_form/2d047129-25c7-4b6b-ad6e-f5fe52f2f76d/{lot_id}/edit",
+                		"viewUrl": "https://df-app-stage.uniproof.com.br/dynamic_form/2d047129-25c7-4b6b-ad6e-f5fe52f2f76d/{lot_id}/view",
+                		"validateUrl": "https://df-api-stage.uniproof.com.br/templates/2d047129-25c7-4b6b-ad6e-f5fe52f2f76d/forms/{lot_id}/status",
+                		"dataUrl": "https://df-api-stage.uniproof.com.br/templates/2d047129-25c7-4b6b-ad6e-f5fe52f2f76d/forms/{lot_id}",
+                		"copyUrl": "https://df-api-stage.uniproof.com.br/forms/2d047129-25c7-4b6b-ad6e-f5fe52f2f76d/copy",
+                		"helpUrl": null,
+                		"active": true,
+                		"onlyPdf": false,
+                		"onDemand": true,
+                		"requireFile": true,
+                		"requireSignature": false,
+                		"options": {
+                			"formWidth": "900",
+                			"formHeight": "750",
+                			"formMarginTop": "-10",
+                			"formMarginLeft": "0",
+                			"folderDisclamer": "Atenção: Preenchendo o campo CPF/CNPJ do Centro de custo, o faturamento será feito para este Centro de Custo indicado. Do contrário será faturado para a empresa que está solicitando o pedido",
+                			"formMarginRight": "0",
+                			"formMarginBottom": "0"
+                		},
+                		"initialStatus": "created"
+                	},
+                	"city": {
+                		"id": 5200050,
+                		"name": "Abadia de Goiás",
+                		"state": "GO"
+                	},
+                	"document": {
+                		"id": "2bd62bdb-6de5-4867-93c2-66a1f402c87d",
+                		"containerId": "06e76c96-1cfe-4ff1-bd14-41853450572d",
+                		"lastEventId": 8029133,
+                		"extension": ".json",
+                		"mimetype": "application/json",
+                		"name": "REGISTRO DE DOCUMENTOS NATO DIGITAIS JÁ ASSINADOS - RTD 13 de fevereiro 16:14.json",
+                		"pages": 0,
+                		"signed": false,
+                		"visible": true,
+                		"totalSize": "0",
+                		"currentVersion": 0,
+                		"archivedAt": null,
+                		"createdAt": "2025-02-13T19:14:53.211Z",
+                		"updatedAt": "2025-02-13T19:14:53.283Z",
+                		"creator": {
+                			"id": 2,
+                			"blockCompany": false,
+                			"name": "User 2",
+                			"shortName": null,
+                			"email": "user2@uniproof.com.br",
+                			"login": "user2@uniproof.com.br",
+                			"cpf": "00000000000",
+                			"origin": null,
+                			"skipPasswordExpiration": false,
+                			"sso": false,
+                			"expiresPasswordAt": null,
+                			"otpBase32": "MO6PIEWEJZREPFSUKNC2ZUDU",
+                			"otpEnabled": false
+                		}
+                	},
+                	"attachments": [
+                		{
+                			"id": "71ce520b-bbb5-4377-b6c1-579eb04671d7",
+                			"ownerType": "LotItem",
+                			"ownerId": "7738edfa-7862-4058-b0f6-7c9aabbd2a78",
+                			"documentId": "813f9daa-f2a2-4231-9982-f7d0fc1f066f",
+                			"attachmentTypeId": 7,
+                			"parentId": null,
+                			"currentVersion": 1,
+                			"removedAt": null,
+                			"expiresAt": null,
+                			"createdAt": "2025-02-13T19:17:22.672Z",
+                			"updatedAt": "2025-02-13T19:17:22.672Z",
+                			"document": {
+                				"id": "813f9daa-f2a2-4231-9982-f7d0fc1f066f",
+                				"containerId": "06e76c96-1cfe-4ff1-bd14-41853450572d",
+                				"lastEventId": null,
+                				"extension": ".pdf",
+                				"mimetype": "application/pdf",
+                				"name": "assinatura_egov.pdf",
+                				"pages": 1,
+                				"signed": false,
+                				"visible": true,
+                				"totalSize": "304660",
+                				"currentVersion": 1,
+                				"archivedAt": null,
+                				"createdAt": "2025-02-13T19:17:22.625Z",
+                				"updatedAt": "2025-02-13T19:17:22.717Z"
+                			},
+                			"attachmentType": {
+                				"id": 7,
+                				"name": "attachment",
+                				"label": "Anexo",
+                				"notaryLabel": "Anexo",
+                				"active": true,
+                				"deletable": true,
+                				"visible": true,
+                				"forwardable": false,
+                				"registered": false,
+                				"icon": "paperclip",
+                				"color": "rgb(59, 114, 231)",
+                				"expirationDays": null,
+                				"sortOrder": 0
+                			}
+                		},
+                		{
+                			"id": "86226517-137f-44cb-a30e-970293541085",
+                			"ownerType": "LotItem",
+                			"ownerId": "7738edfa-7862-4058-b0f6-7c9aabbd2a78",
+                			"documentId": "4d9ba5ff-8cae-4b7c-832d-248594b8b636",
+                			"attachmentTypeId": 10,
+                			"parentId": "813f9daa-f2a2-4231-9982-f7d0fc1f066f",
+                			"currentVersion": 1,
+                			"removedAt": "2025-02-20T21:06:29.280Z",
+                			"expiresAt": null,
+                			"createdAt": "2025-02-13T20:52:02.447Z",
+                			"updatedAt": "2025-02-20T21:06:29.285Z",
+                			"document": {
+                				"id": "4d9ba5ff-8cae-4b7c-832d-248594b8b636",
+                				"containerId": "06e76c96-1cfe-4ff1-bd14-41853450572d",
+                				"lastEventId": null,
+                				"extension": ".pdf",
+                				"mimetype": "application/pdf",
+                				"name": "uniproof-assinatura_egov.pdf",
+                				"pages": 2,
+                				"signed": false,
+                				"visible": false,
+                				"totalSize": "521574",
+                				"currentVersion": 1,
+                				"archivedAt": null,
+                				"createdAt": "2025-02-13T20:52:02.418Z",
+                				"updatedAt": "2025-02-13T20:52:02.480Z"
+                			},
+                			"attachmentType": {
+                				"id": 10,
+                				"name": "uniproof_signed",
+                				"label": "Assinado Uniproof",
+                				"notaryLabel": "Assinado Uniproof",
+                				"active": true,
+                				"deletable": false,
+                				"visible": false,
+                				"forwardable": true,
+                				"registered": false,
+                				"icon": "signature",
+                				"color": "rgb(59, 114, 231)",
+                				"expirationDays": null,
+                				"sortOrder": 30
+                			}
+                		}
+                	],
+                	"lot": {
+                		"id": "e9cf4553-2a9e-4a41-a1eb-0ba546efcb1e",
+                		"lastEventId": 8029131,
+                		"creatorId": 2,
+                		"name": "Proc: 1739474093152",
+                		"description": "",
+                		"containerId": "06e76c96-1cfe-4ff1-bd14-41853450572d",
+                		"serviceId": 3,
+                		"cityId": 5200050,
+                		"priceId": 1921,
+                		"price": "0.00",
+                		"createdAt": "2025-02-13T19:14:53.157Z",
+                		"updatedAt": "2025-02-27T18:45:59.022Z",
+                		"event": {
+                			"id": 8029131,
+                			"userId": 2,
+                			"notaryId": null,
+                			"ownerType": "Lot",
+                			"ownerId": "e9cf4553-2a9e-4a41-a1eb-0ba546efcb1e",
+                			"status": "draft",
+                			"description": null,
+                			"archivedAt": null,
+                			"archiverId": null,
+                			"createdAt": "2025-02-13T19:14:53.193Z",
+                			"updatedAt": "2025-02-13T19:14:53.193Z"
+                		}
+                	},
+                	"container": {
+                		"id": "06e76c96-1cfe-4ff1-bd14-41853450572d",
+                		"name": "2342 - Morada da Serra",
+                		"description": "23423423424234",
+                		"sequentialId": 7155,
+                		"tooltip": null
+                	},
+                	"blockchain": null,
+                	"tags": []
+                }
+                """;
 
-        String json = "{" +
-                "\"id\": \"914f5ac3-3194-46ae-94fa-ba2f2a41ddee\"," +
-                "\"lotId\": \"11987d8e-9a5c-48a6-b54d-e224b901b7b8\"," +
-                "\"parentId\": null," +
-                "\"protocol\":  null," +
-                "\"protocol1\":  \"CDTSP 20220721105984\"," +
-                "\"sellingPrice\": \"194.69\"," +
-                "\"notaryPrice\": \"135.29\"," +
-                "\"currentVersion\": 0," +
-                "\"registeredVersion\": 2," +
-                "\"archivedAt\": null," +
-                "\"createdAt\": \"2022-05-25T19:36:43.191Z\"," +
-                "\"updatedAt\": \"2022-05-31T20:21:33.508Z\"," +
-                "\"event\": {" +
-                "\"id\": 1909113," +
-                "\"userId\": 477," +
-                "\"ownerType\": \"LotItem\"," +
-                "\"ownerId\": \"914f5ac3-3194-46ae-94fa-ba2f2a41ddee\"," +
-                "\"status\": \"canceled\"," +
-                "\"description\": \"<p><br></p>\\n<p><br></p>\"," +
-                "\"manual\": true," +
-                "\"createdAt\": \"2022-05-31T20:21:33.489Z\"," +
-                "\"updatedAt\": \"2022-05-31T20:21:33.489Z\"" +
-                "}," +
-                "\"owner\": {" +
-                "\"id\": 4," +
-                "\"token\": \"217a43e6-50d6-4121-a8ae-394e9fa54a25\"," +
-                "\"userId\": null," +
-                "\"kind\": \"company\"," +
-                "\"standard\": false," +
-                "\"cityId\": 3550308," +
-                "\"name\": \"Uniproof\"," +
-                "\"fantasy\": \"Aureo Alto Astral\"," +
-                "\"phone\": null," +
-                "\"shortName\": \"Uniproof\"," +
-                "\"cnpj\": \"29.383.051/0001-21\"," +
-                "\"email\": null," +
-                "\"onlyCommercialAddress\": false," +
-                "\"endContractTerm\": null," +
-                "\"startContractTerm\": null," +
-                "\"comments\": null," +
-                "\"createdAt\": \"2018-05-24T18:10:40.176Z\"," +
-                "\"updatedAt\": \"2018-05-24T18:10:40.176Z\"" +
-                "}," +
-                "\"service\": {" +
-                "\"id\": 62," +
-                "\"name\": \"REGISTROS DE ATAS DIGITALIZADAS - RTD\"," +
-                "\"alias\": \"Referente a Ata Condominial\"," +
-                "\"description\": \"Registro de Atas Condominiais digitalizadas através de scanner ou foto.\"," +
-                "\"fullDescription\": \"Registro de Atas Condominiais digitalizadas através de scanner ou foto.\"," +
-                "\"helpUrl\": null," +
-                "\"onDemand\": false," +
-                "\"requireFile\": true," +
-                "\"enableCity\": true" +
-                "}," +
-                "\"city\": {" +
-                "\"id\": 5200050," +
-                "\"name\": \"Abadia de Goiás\"," +
-                "\"state\": \"GO\"" +
-                "}," +
-                "\"document\": {" +
-                "\"id\": \"b17c09fa-966a-47f3-9735-51611fc1e81f\"," +
-                "\"containerId\": null," +
-                "\"lastEventId\": 1889195," +
-                "\"extension\": \".json\"," +
-                "\"mimetype\": \"application/json\"," +
-                "\"name\": \"REGISTROS DE ATAS DIGITALIZADAS - RTD 25 de maio 16:36.json\"," +
-                "\"pages\": 7," +
-                "\"signed\": false," +
-                "\"visible\": true," +
-                "\"totalSize\": \"0\"," +
-                "\"currentVersion\": 2," +
-                "\"archivedAt\": null," +
-                "\"createdAt\": \"2022-05-25T19:36:43.181Z\"," +
-                "\"updatedAt\": \"2022-05-26T20:24:02.755Z\"," +
-                "\"creator\": {" +
-                "\"id\": 477," +
-                "\"name\": \"Amanda Pereira de Freitas\"," +
-                "\"shortName\": null," +
-                "\"email\": \"adm@uniproof.com.br\"," +
-                "\"cpf\": \"36124278812\"," +
-                "\"origin\": null" +
-                "}" +
-                "}," +
-                "\"attachments\": []," +
-                "\"lot\": {" +
-                "\"id\": \"11987d8e-9a5c-48a6-b54d-e224b901b7b8\"," +
-                "\"lastEventId\": null," +
-                "\"name\": null," +
-                "\"name1\": \"Lote 25 de maio 16:36\"," +
-                "\"description\": \"\"," +
-                "\"containerId\": null," +
-                "\"serviceId\": 62," +
-                "\"cityId\": 5200050," +
-                "\"priceId\": 1923," +
-                "\"price\": \"194.69\"," +
-                "\"createdAt\": \"2022-05-25T19:36:43.171Z\"," +
-                "\"updatedAt\": \"2022-05-26T20:24:00.203Z\"," +
-                "\"event\": null" +
-                "}," +
-                "\"blockchain\": {" +
-                "\"id\": 63357," +
-                "\"protocol\": \"0x863ecbd61966899d7207f1df879e8c3c3c4ba0ca0124de1982d096e7918d5215\"," +
-                "\"validationCode\": \"0x863ecbd61966899d7207f1df879e8c3c3c4ba0ca0124de1982d096e7918d5215\"," +
-                "\"notaryName\": \"https://blockscout.com/etc/mainnet/tx/0x863ecbd61966899d7207f1df879e8c3c3c4ba0ca0124de1982d096e7918d5215\"," +
-                "\"registryDate\": \"2022-05-26T20:30:42.000Z\"," +
-                "\"createdAt\": \"2022-05-25T19:36:43.191Z\"," +
-                "\"updatedAt\": \"2022-05-26T20:36:02.977Z\"" +
-                "}" +
-                "}";
-        String texto =
+        String texto1 =
                 "${owner.fantasy:-${owner.name}} - ${lot.name:-Avulso} - ${protocol:-Sem protocolo}\n" +
                         "LotItem: ${id}\n${wallet.ownerType}_${container.id:-${owner.token}}\n" +
                         "Este documento pode ser pago antes ou depois da data de seu vencimento. Prefira Pix.\n" +
                         "O processo de REGISTRO SERÁ INICIADO QUANDO COMPENSADO, pelo banco.";
 
+        String texto =
+                "${owner.fantasy:-${owner.name}} - ${lot.name:- }\n" +
+                        "LotItem: ${id}\n"+
+                        "${container.name}\n" +
+                        "Este documento pode ser pago antes ou depois da data de seu vencimento. Prefira Pix.";
+        //+                        "O processo de REGISTRO SERÁ INICIADO QUANDO COMPENSADO, pelo banco.";
 
         System.out.println(replaceString(texto, json));
-        System.out.println(StringUtils.getNestedProperty("owner.name1.alias.juca", convertObjToMap(json)));
+
+
+        String[] test = {
+           "2342 - Morada da Serra",
+           "B2Br - Empresa X",
+           "2-Morada da Serra",
+           "Morada da Serra - office",
+                "31.454.503/0001-60 - Juca",
+                "010001 0599-00 - 010006 0597-00 - LETICIA DE PAULA SOUZA MORAES",
+                "CCE048237-7 REQUERIMENTO - 90.608.084/0001-33",
+                "689 - CONDOMINIO RESIDENCIAL GARDEN - CNPJ:58.223.018/0001-32"
+        } ;
+
+        System.out.println("\n\n\n");
+        for (String s : test) {
+            System.out.println(s + " /-> " + s.replaceFirst("^[0-9,. \\\\/-]*",""));
+        }
+        //System.out.println(StringUtils.getNestedProperty("owner.name1.alias.juca", convertObjToMap(json)));
     }
 
     public static String replaceString(String input, Object context) throws JsonProcessingException {
